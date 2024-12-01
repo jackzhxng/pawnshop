0a53d7bc21529757da3fb638e5f9dabfbec362e9
Edward Zhang <edwardz123@gmail.com>
2024-12-01 10:39:14 -0500

    clean up messy code

--- server/llm/chat_no_langchain_tool_call.py
+++ server/llm/chat_no_langchain_tool_call.py
@@ -1,24 +1,21 @@
-from langchain_core.messages.tool import tool_call
 from llama_cpp import ChatCompletionRequestAssistantMessage, ChatCompletionRequestMessage, ChatCompletionRequestSystemMessage, ChatCompletionRequestUserMessage, Llama
 from typing import List
-from prompts import system_prompt, tool_calling_prompt
+from prompts import system_prompt 
+
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 # From https://github.com/abetlen/llama-cpp-python
 llm = Llama(
-      model_path="../models/Llama-3.2-3B-Instruct-Q5_K_M.gguf", # https://huggingface.co/bartowski/Llama-3.2-3B-Instruct-GGUF
+      model_path="../models/Llama-3.2-3B-Instruct-Q5_K_M.gguf", 
       n_gpu_layers=-1, # Uncomment to use GPU acceleration
       # seed=1337, # Uncomment to set a specific seed
       n_ctx=100000, # Uncomment to increase the context window
       verbose=False,
 )
 
-# ====== Chat generation example =======
-# out = llm.create_completion(
-#   seed=-1,
-#   max_tokens=500,
-#   prompt=f"{system_prompt} \n\nShopkeeper: Hi, how are you? How can I help you? \n\nErik:",
-#   stop="Shopkeeper:"
-# )
 
 class LlamaCppAgent:
   # Stores chat history
@@ -28,66 +25,36 @@ class LlamaCppAgent:
     self.llm = llm
     self.messages = []
 
+  def get_messages_with_system_prompts(self, system_prompts: List[str]):  
+    """
+    Returns the message history with the specified system prompt prepended to it.
+    Allows us to dynamically switch out system prompts
+    """
+    messages_with_prompt: List[ChatCompletionRequestMessage] = [
+      ChatCompletionRequestSystemMessage(role="system", content=system_prompt) 
+      for system_prompt in system_prompts
+    ]
+    messages_with_prompt.extend(self.messages)
+    return messages_with_prompt
+
+
   def generate_chat(self, input_message: str):
     """
     What the user uses - has the agent do everything it needs to do
     (LLM queries, tool calls, etc.) in order to arrive at a suitable
     response to the user.
     """
-    print("Calling generate_chat...")
+    logger.debug(f"Generating responses with messages: {self.messages}")
     self.messages.append(ChatCompletionRequestUserMessage(role="user", content=input_message))
-    # message_history = '\n\n'.join(self.messages)
-    # prompt = f"{system_prompt} \n\n{message_history} \n\nErik:"
-    # out = llm.create_completion(
-    #   seed=-1,
-    #   max_tokens=500,
-    #   prompt=prompt,
-    #   stop="\n"
-    # )
-
-    messages_with_prompt = [ChatCompletionRequestSystemMessage(role="system", content=system_prompt)]
-    messages_with_prompt.extend(self.messages)
-    out = llm.create_chat_completion(
-      seed=-1,
+    messages_with_prompt = self.get_messages_with_system_prompts([system_prompt])
+    out = llm.create_chat_completion( seed=-1, # Sets random seed every time, so makes completion non-deterministic
       messages=messages_with_prompt
     )
-    # print(f"[DEBUG - prompt]: {prompt}")
-    print("======================================")
-    print(out)
+    logger.debug(out)
+
     agent_message = out["choices"][0]["message"]
     self.messages.append(ChatCompletionRequestAssistantMessage(role="assistant", content=agent_message))
     return self.messages[-1]
-
-  def generate_tool_call(self):
-    messages_with_prompt = [ChatCompletionRequestSystemMessage(role="system", content=tool_calling_prompt)]
-    messages_with_prompt.extend(self.messages)
-    out = llm.create_chat_completion(
-      seed=-1,
-      max_tokens=500,
-      messages=messages_with_prompt,
-      tools=[{
-          "type": "function",
-          "function": {
-              "name": "Sell",
-              "parameters": {
-                  "type": "object",
-                  "title": "Sell item",
-                  "properties": {
-                      "name": {
-                          "title": "Name",
-                          "type": "string"
-                      },
-                      "price": {
-                          "title": "Price",
-                          "type": "integer"
-                      }
-                  },
-                  "required": [ "name", "price" ]
-              }
-          }
-      }]
-    )
-    print(out)
 
 
 if __name__ == "__main__":
@@ -107,9 +74,6 @@ if __name__ == "__main__":
       except Exception as e:
           print(f"An error occurred: {str(e)}")
 
-      tool_response = agent.generate_tool_call()
-      print(tool_response)
-
   except KeyboardInterrupt:
     print("\nChat session interrupted by user.")
   except Exception as e:
@@ -117,11 +81,7 @@ if __name__ == "__main__":
 
  
 
-
-
-
 #  ============ Function calling example ============
-
 # out = llm.create_chat_completion(
 #   seed=-1,
 #   messages = [
@@ -166,3 +126,10 @@ if __name__ == "__main__":
 # print(out)
 # print(out["choices"][0]["message"])
 
+# ====== Chat generation example =======
+# out = llm.create_completion(
+#   seed=-1,
+#   max_tokens=500,
+#   prompt=f"{system_prompt} \n\nShopkeeper: Hi, how are you? How can I help you? \n\nErik:",
+#   stop="Shopkeeper:"
+# )
